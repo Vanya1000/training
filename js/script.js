a639869d4c144b77afffe86a6ce827189beeeff7
Vanya1000 <Vanya1000@yandex.ru>
2021-10-19 19:44:20 +0300

    save

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 "use strict"
-let i = 0;
-for ( i = 0; i < 31; i++) { // выведет 0, затем 1, затем 2
-	console.log (i)
-}
-alert(i);
+let userName = 22;
+console.log(typeof userName);
+let i = 20 > 18;
+console.log(i)
+
+const bigInt = 123n;
+console.log(typeof bigInt);
+
