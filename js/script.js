f9c29b5c77d4cb6bad68fc46f635e7ae35299db5
Vanya1000 <Vanya1000@yandex.ru>
2021-10-19 20:08:09 +0300

    save

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ console.log(i)
 const bigInt = 123n;
 console.log(typeof bigInt);
 
+let userAge = 36;
+userAge = String(userAge);
+let userAgeInfo = `возраст: ${userAge}`;
+console.log(userAgeInfo);
+let userInfo = {
+	name: 'Ivan',
+	age: 29
+}
