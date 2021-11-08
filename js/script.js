a3ee36b7fc6a2b394fcbd210ce96acf902b45164
Vanya1000 <Vanya1000@yandex.ru>
2021-11-08 12:06:15 +0300

    12_06

--- js/script.js
+++ js/script.js
@@ -1239,4 +1239,74 @@ const mainFormInput = mainForm.nameInput;
 mainFormInput.addEventListener("input", function (event) {
 	console.log(`value: ${mainFormInput.value}`);
 });
+
+const mainForm = document.forms.main;
+const mainFormInput = mainForm.nameInput;
+
+mainFormInput.addEventListener("copy", function (event) {
+	console.log(`Копируем`);
+});
+mainFormInput.addEventListener("paste", function (event) {
+	console.log(`Вставляем`);
+	event.preventDefault();
+});
+mainFormInput.addEventListener("cut", function (event) {
+	console.log(`Вырезаем`);
+});
+
+const mainForm = document.forms.main;
+const mainFormInput = mainForm.nameInput;
+
+mainForm.addEventListener("submit", function (event) {
+	console.log('Форма отправляется...');
+
+	// Проверяем поля и если есть ошибки отменяем отправку
+	if (!mainFormInput.value) {
+		console.log('Поле nameInput не заполнено');
+		event.preventDefault();
+	}
+});
+
+const mainForm = document.forms.main;
+const mainFormInput = mainForm.nameInput;
+
+mainForm.addEventListener("submit", function (event) {
+	if (emailTest(mainFormInput)) {
+		mainFormInput.parentElement.insertAdjacentHTML(
+			"beforeend",
+			`<div class="main-form__error">
+				Введите email
+			</div>`
+		);
+		event.preventDefault();
+	}
+});
+
+mainFormInput.addEventListener("focus", function (event) {
+	if (mainFormInput.nextElementSibling) {
+		mainFormInput.nextElementSibling.remove();
+	}
+});
+
+//Функция теста email
+function emailTest(input) {
+	return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
 */
+// Выводим картинку после выбора
+const mainForm = document.forms.main;
+const mainFormFile = mainForm.nameFile;
+
+mainFormFile.addEventListener("change", function (e) {
+	let selectedFile = mainFormFile.files[0];
+
+	// Получаем URL изображения
+	let fileUrl = URL.createObjectURL(selectedFile);
+
+	mainFormFile.parentElement.insertAdjacentHTML(
+		"beforeend",
+		`<div class="main-form__image">
+			<img alt="" title="${selectedFile.name}" src="${fileUrl}">
+		</div>`
+	);
+});
