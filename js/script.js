c97b48121cfdec6fbf701e698c868b3c76b72a8d
Vanya1000 <Vanya1000@yandex.ru>
2021-11-26 11:24:35 +0300

    11_24

--- js/script.js
+++ js/script.js
@@ -2477,7 +2477,7 @@ console.log(student.test); //При попытке обратиться пуст
 
 
 console.log(student);
-*/
+
 // !Статические методы и свойства (без создания объекта)
 class User {
 	constructor(name) {
@@ -2490,3 +2490,60 @@ class User {
 }
 
 console.log(User.getRole('Ivan@ex'));
+*/
+
+class Dog {
+	constructor(name, breed, weight) {
+		this.name = name;
+		this.breed = breed;
+		this.weight = weight;
+		this.species = 'Canine';
+	}
+	bark() {
+		if (this.weight > 25) {
+			console.log(this.name + ' say Woff!');
+		} else {
+			console.log(this.name + ' say Yip!');
+		}
+	}
+	run() {
+		console.log('Run!');
+	}
+	wag() {
+		console.log('Wag!');
+	}
+}
+
+let fido = new Dog('Fido', 'Mixed', 38);
+console.log(fido);
+fido.bark();
+
+class ShowDog extends Dog { //ShowDog расширяет Dog
+	constructor(name, bread, weight, handler) { // функция конструктор
+		super(name, bread, weight); //метод super посылает родителю то что он ждет. Обязательно!
+		this.handler = handler;
+		this.league = 'Webville';
+	}
+	stack() {
+		console.log("Stack");
+	}
+	bait() {
+		console.log("Bait");
+	}
+	gait(kind) {
+		console.log(kind + "ing");
+	}
+	groom() {
+		console.log("Groom");
+	}
+	run() {
+		super.run();// Можем обращаться к родительскому методу. Мтод super это просто обращ к прототипу!
+		console.log('ShowRun!'); // Можно перезаписавать св-ва и!
+	}
+}
+
+let scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+console.log(scotty);
+let beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
+
+beatrice.run();
