7de787d6eb8cb2ee186c8ab995e581184d8652b3
Vanya1000 <Vanya1000@yandex.ru>
2022-06-18 22:51:45 +0300

    add: time and OOP

--- js/script.js
+++ js/script.js
@@ -5959,19 +5959,4 @@ function undoRedo(object) { //good
   };
 } */
 
-let arr = [1, 4, 3, 2];
 
-function bubbleSort(arr) {
-  const n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - 1 - i; j++) {
-      if (arr[j + 1] < arr[j]) {
-        let t = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = t;
-      }
-    }
-  }
-  return arr;
-}
-bubbleSort(arr);
