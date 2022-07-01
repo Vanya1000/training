dfc7a36f022581d6eba3783d4adababaa69bc9a5
Vanya1000 <Vanya1000@yandex.ru>
2022-07-01 23:22:47 +0300

    add: time

--- 1_Materials/JS/promise.js
+++ 1_Materials/JS/promise.js
@@ -1,54 +1,65 @@
 "use strict";
+/* 
++Ways to write asynchronous code / manage async events:
+  -setTimeout() / setInterval()
+  -callbacks(setTimeout, setInterval, onClick, onLoad, onChange, onSubmit...)
+  -Promises, async/await
+  
+Promise:
+  Has three possible states:
+  -pending: initial state, neither fulfilled nor rejected
+  -fulfilled: meaning that the operation completed successfully
+  -rejected: meaning that the operation failed
+*/
+		//? Промисификация – это когда берут асинхронную функциональность и делают для неё обёртку, возвращающую промис.
+		//? «Чейнинг» (chaining), то есть возможность строить асинхронные цепочки из промисов – пожалуй, основная причина, из-за которой существуют и активно используются промисы.
+		doItAfter(2)
+			.then((data) => {
+				console.log(data)
+				return data.slice(0, 2)
+			})//? необходимо явно дальше передать данные по цепочке!!!!!!!! При чейнинге, то есть последовательных вызовах .then…then…then, в каждый следующий then переходит результат от предыдущего.
+			.catch(() => console.log('ошибка')) //? если промис выполнится нормально, то ничего не произойдет, если ошибка, то выполнится этот код | вместо .then(null, onRejected)
+			.then((message) => console.log(message)) //? callback который раньше передавалив функцию, теперь мы вызываем когда промис выполнится
+		
+		
+		
+		
+		function doItAfter(seconds) {  //? создаем промис объект и возвращаем его | callback не нужен теперь!
+			let promise = new Promise((resolve, reject) => { //? я промис и я обещаю выполнить эту функцию 
+				console.log('I am doing something asinc');
+				setTimeout(() => {
+					resolve([1, 2, 3, 4])
+				}, seconds * 1000);
+			})
+		
+			return promise //? без промисов мы не имели права писать return | но теперь мы во внешний мир отдаем обещание выполниться
+		}
 
-//? Промисификация – это когда берут асинхронную функциональность и делают для неё обёртку, возвращающую промис.
-//? «Чейнинг» (chaining), то есть возможность строить асинхронные цепочки из промисов – пожалуй, основная причина, из-за которой существуют и активно используются промисы.
-doItAfter(2)
-	.then((data) => {
-		console.log(data)
-		return data.slice(0, 2)
-	})//? необходимо явно дальше передать данные по цепочке!!!!!!!! При чейнинге, то есть последовательных вызовах .then…then…then, в каждый следующий then переходит результат от предыдущего.
-	.catch(() => console.log('ошибка')) //? если промис выполнится нормально, то ничего не произойдет, если ошибка, то выполнится этот код | вместо .then(null, onRejected)
-	.then((message) => console.log(message)) //? callback который раньше передавалив функцию, теперь мы вызываем когда промис выполнится
+		//?---------------------------------------------------------------------------------------------------------------------
 
+		let promise1 = () => {
+			let promise = new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve()
+				}, 3000);
+			})
+		
+			return promise
+		};
 
+		let promise2 = () => {
+			let promise = new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve()
+				}, 3000);
+			})
+		
+			return promise
+		};
 
+		Promise.all([promise1(), promise2()]).then(() => console.log('all done')); //? выполнится последовательно по очереди
 
-function doItAfter(seconds) {  //? создаем промис объект и возвращаем его | callback не нужен теперь!
-	let promise = new Promise((resolve, reject) => { //? я промис и я обещаю выполнить эту функцию 
-		console.log('I am doing something asinc');
-		setTimeout(() => {
-			resolve([1, 2, 3, 4])
-		}, seconds * 1000);
-	})
-
-	return promise //? без промисов мы не имели права писать return | но теперь мы во внешний мир отдаем обещание выполниться
-}
-
-//?---------------------------------------------------------------------------------------------------------------------
-
-let promise1 = () => {
-	let promise = new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve()
-		}, 3000);
-	})
-
-	return promise
-};
-
-let promise2 = () => {
-	let promise = new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve()
-		}, 3000);
-	})
-
-	return promise
-};
-
-Promise.all([promise1(), promise2()]).then(() => console.log('all done')); //? выполнится последовательно по очереди
-//!---------------------------------------------------------------------------------------------------------------------
-
+// +Async/Await:
 
 doItAfterAsync(4)
 
