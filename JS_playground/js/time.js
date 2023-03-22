2799a6025805bb608e7ebcdbf1d7aa75da971982
Vanya1000 <Vanya1000@yandex.ru>
2023-03-22 23:12:54 +0300

    add: time

--- JS_playground/js/time.js
+++ JS_playground/js/time.js
@@ -482,6 +482,7 @@ function howManyHoursOfTraining() {
     "19.03": 8,
     "10.03": 8,
     "11.03": 10,
+    "12.03": 8,
   };
   let workTimeArray = Object.values(workTime);
   let allHourWorkTime = workTimeArray.reduce(
@@ -1183,6 +1184,10 @@ const countTime = [
   {
     date: "18.03.2022",
     time: 1,
+  },
+  {
+    date: "22.03.2022",
+    time: 2,
   }
 ];
 
